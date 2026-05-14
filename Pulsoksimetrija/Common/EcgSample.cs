897cfca2e6ccabde76c0d4bf508c5279631cb8b8
Strahinja Acic <strahinja.acic@gmail.com>
2026-05-14 13:41:40 +0200

    EcgSample dodata klasa

---
+++ Pulsoksimetrija/Common/EcgSample.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class EcgSample
+    {
+        [DataMember] public long TimestampMs { get; set; }
+        [DataMember] public double? EcgMicroV { get; set; }
+        [DataMember] public double? HeartRate { get; set; }
+        [DataMember] public double? IBI_ms { get; set; }
+        [DataMember] public double? AccX { get; set; }
+        [DataMember] public double? AccY { get; set; }
+        [DataMember] public double? AccZ { get; set; }
+        [DataMember] public string ParticipantId { get; set; }
+        [DataMember] public int RowIndex { get; set; }
+    }
+}
