ba61f1e690e9f98536af65c95e546308bf445a6f
plelimarko <markopleli@gmail.com>
2026-06-14 19:58:39 +0200

    namespace fix

--- Pulsoksimetrija/Client/Program.cs
+++ Pulsoksimetrija/Client/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Common;
-using Common1;
 using System;
 using System.IO;
 using System.ServiceModel;
--- Pulsoksimetrija/Server/EcgService.cs
+++ Pulsoksimetrija/Server/EcgService.cs
@@ -1,5 +1,4 @@
 ﻿using Common;
-using Common1;
 using System;
 using System.Collections.Generic;
 using System.IO;
